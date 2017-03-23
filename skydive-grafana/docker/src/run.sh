865590fbe6ea81abf6ab2f8d4223f8ff3dd6a9c0
Marcos Entenza <mak@redhat.com>
2017-03-23 14:01:24 +0000

    add skydive-grafana project

---
+++ skydive-grafana/docker/src/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+: "${GF_PATHS_DATA:=/var/lib/grafana}"
+: "${GF_PATHS_LOGS:=/var/log/grafana}"
+: "${GF_PATHS_PLUGINS:=/var/lib/grafana/plugins}"
+
+#chown -R grafana:grafana "$GF_PATHS_DATA" "$GF_PATHS_LOGS"
+#chown -R grafana:grafana /etc/grafana
+
+exec /usr/sbin/grafana-server      \
+  --homepath=/usr/share/grafana                 \
+  --config=/etc/grafana/grafana.ini             \
+  cfg:default.paths.data="$GF_PATHS_DATA"       \
+  cfg:default.paths.logs="$GF_PATHS_LOGS"       \
+  cfg:default.paths.plugins="$GF_PATHS_PLUGINS" \
+  "$@"
