5afbed3d469834f4fdd3caf207df60083f3e5e9c
Eric Sauer <esauer@redhat.com>
2019-06-19 20:31:43 -0400

    Refactor and tests

---
+++ _test/setup.sh
@@ -0,0 +1,53 @@
+
+cluster_up() {
+  set +e
+  built=false
+  while true; do
+    DEV=$(ip link | awk '/state UP/{ gsub(":", ""); print $2}')
+    IP_ADDR=$(ip addr show $DEV | awk '/inet /{ gsub("/.*", ""); print $2}')
+    oc cluster up --public-hostname=${IP_ADDR} --routing-suffix=${IP_ADDR}.nip.io --base-dir=$HOME/ocp
+    if [ "$?" -eq 0 ]; then
+      built=true
+      break
+    fi
+    echo "Retrying oc cluster up after failure"
+    oc cluster down
+    sleep 5
+  done
+  echo "OpenShift Cluster Running"
+}
+
+setup() {
+  echo "${TRAVIS_BRANCH:=master}"
+  echo "${TRAVIS_REPO_SLUG:=redhat-cop/containers-quickstarts}"
+  ansible-galaxy install -r jenkins-slaves/requirements.yml -p galaxy
+  ansible-playbook -i jenkins-slaves/.applier/ galaxy/openshift-applier/playbooks/openshift-cluster-seed.yml -e namespace=containers-quickstarts-tests -e slave_repo_ref=${TRAVIS_BRANCH} -e repository_url=https://github.com/${TRAVIS_REPO_SLUG}.git
+}
+
+test() {
+  # Wait for builds to start
+  while [ "$(oc get builds)" == "" ]; do
+    sleep 1
+  done
+
+  # Wait for all builds to complete
+  while [ "$(oc get builds -o jsonpath='{}')" != "" ]; do
+    sleep 1
+  done
+}
+
+# Process arguments
+case $1 in
+  cluster_up)
+    cluster_up
+    ;;
+  setup)
+    setup
+    ;;
+  test)
+    test
+    ;;
+  *)
+    echo "Not an option"
+    exit 1
+esac
