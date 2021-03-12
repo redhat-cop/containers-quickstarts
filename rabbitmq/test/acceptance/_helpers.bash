cf65853b74f7d38b1ac31a8ed1ad0fa0294c506a
Petter Abrahamsson <petter@redhat.com>
2021-03-12 04:20:45 -0500

    Migrate to Bats & Helm for testing (#454)

--- rabbitmq/test/acceptance/_helpers.bash
+++ rabbitmq/test/acceptance/_helpers.bash
@@ -6,6 +6,31 @@ chart_dir() {
   echo ${BATS_TEST_DIRNAME}/../../chart
 }
 
+# Create/Delete project
+project_action() {
+  ACTION=$1
+  PROJECT="rabbitmq-acceptance-${2}"
+
+  if [ "${ACTION}" == "create" ]; then
+    oc new-project ${PROJECT}
+  elif [ "${ACTION}" == "delete" ]; then
+    oc delete pod -l app.kubernetes.io/name=$(name_prefix) --ignore-not-found=true
+    oc delete pvc -l app.kubernetes.io/name=$(name_prefix) --ignore-not-found=true
+    oc delete project ${PROJECT}
+  fi
+}
+
+# If running inside a pod, authenticate to k8s/ocp
+ocp_auth() {
+  SA_PATH=/run/secrets/kubernetes.io/serviceaccount
+  if [[ -f ${SA_PATH}/token && -n "${OCP_AUTH}" ]]; then
+    export KUBECONFIG=/tmp/.kube/config
+    oc login --token $(cat ${SA_PATH}/token) \
+      --server https://kubernetes.default.svc \
+      --certificate-authority ${SA_PATH}/ca.crt
+  fi
+}
+
 # wait for a pod to be ready
 wait_for_running() {
     POD_NAME=$1
